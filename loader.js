ed085e0c4828d1c7762c5c398c98a2aedffbceae
dipesh-mvpl <dipesh.c@microvistatech.in>
2026-01-30 16:04:06 +0530

    Update script source URL to use refs/heads

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 (function () {
   const s = document.createElement("script");
-  s.src = "https://raw.githubusercontent.com/dipesh-mvpl/HRMS-Time/main/hrmsScript.js?ts=" + Date.now();
+  s.src = "https://raw.githubusercontent.com/dipesh-mvpl/HRMS-Time/refs/heads/main/hrmsScript.js?ts=" + Date.now();
   document.body.appendChild(s);
 })();
