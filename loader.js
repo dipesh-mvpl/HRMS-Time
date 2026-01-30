fe41ee872c2b213136c0bb969571a774353ac836
dipesh-mvpl <dipesh.c@microvistatech.in>
2026-01-30 15:59:12 +0530

    Add loader.js to dynamically load hrmsScript.js

---
+++ loader.js
@@ -0,0 +1,5 @@
+(function () {
+  const s = document.createElement("script");
+  s.src = "https://raw.githubusercontent.com/dipesh-mvpl/HRMS-Time/main/hrmsScript.js?ts=" + Date.now();
+  document.body.appendChild(s);
+})();
